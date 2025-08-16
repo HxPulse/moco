21eb32b642ae37c57c825a7b5910ffd1e7db453d
HxPulse <mattadam01@yahoo.fr>
2025-08-16 14:07:01 +0200

    #46 - updated images & modleZ zoom 75%

--- js/modleZ.js
+++ js/modleZ.js
@@ -22,7 +22,7 @@ const guessesContainer = document.getElementById("guesses");
 const zoomedImage = document.getElementById("zoomed-image-wrapper");
 const imgPath = `assets/images/items/${itemToGuess.name.replace(/[^a-z0-9]/gi, "_")}.png`;
 let currentZoom = 900; // starting at 1000%
-const zoomStep = 70;   // decrease by 100% on each wrong guess
+const zoomStep = 75;   // decrease by 100% on each wrong guess
 const minZoom = 100;    // minimum zoom 100%
 
 // Random offset between 0% and 90% (so the zoom box doesn't go out of bounds)
